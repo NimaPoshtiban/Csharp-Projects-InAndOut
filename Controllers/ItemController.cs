3c2e50c372ee1e9d4c2ab3ec7d677cfba61dd2d3
Nima Poshtiban <poshtibannima@gmail.com>
2021-08-13 22:05:07 +0430

    added Create controller + view

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +10,18 @@ namespace InAndOut.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            IEnumerable<Item> objectList = _db.Items;
+            return View(objectList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
