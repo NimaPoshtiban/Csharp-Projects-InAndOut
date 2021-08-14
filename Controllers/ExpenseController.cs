f7f0b08da5befdb25d52bf1cfcd420550307cbff
Nima Poshtiban <poshtibannima@gmail.com>
2021-08-14 11:37:33 +0430

    added expense controller + view + model

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,38 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public ExpenseController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objectList = _db.Expenses;
+            return View(objectList);
+        }
+        // Get-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // Post-Create 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
